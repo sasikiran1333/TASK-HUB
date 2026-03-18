b85410ecf685d2b4ff7ef9d24faef5cb535cee99
prasad <prasad022514@gmail.com>
2026-03-18 14:06:16 +0530

    update

---
+++ scrpit.js
@@ -0,0 +1,10 @@
+/*login buttton in main page*/
+function toggleMenu(){
+    var menu = document.getElementById("dropdown");
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
+/*           end            */
