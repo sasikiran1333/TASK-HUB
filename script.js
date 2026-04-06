5097ed2b4e4d5b43a6c84fb22b8199fa760de99c
prasad <prasad022514@gmail.com>
2026-04-06 13:44:08 +0530

    Added login page

---
+++ script.js
@@ -0,0 +1,58 @@
+function show(icon){
+let pass=icon.previousElementSibling;
+    if(pass.type==="password"){
+        pass.type="text";
+        icon.innerHTML="🔓";
+    }else{
+        pass.type="password";
+        icon.innerHTML="🔒";
+
+    }
+    }
+
+
+function pass(){
+    let pa=document.getElementById("pass1");
+    let ps=document.getElementById("pass2");
+    if (pa.value===ps.value){
+        return true;
+    }else{
+        alert("passwords do not match");
+            return false;
+    }
+}
+
+
+let captchaText = "";
+
+function generateCaptcha(){
+let canvas = document.getElementById("captcha");
+let ctx = canvas.getContext("2d");
+
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+captchaText = "";
+
+for(let i=0;i<5;i++){
+captchaText += chars[Math.floor(Math.random()*chars.length)];
+}
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ctx.font = "22px Arial";
+ctx.fillStyle = "black";
+ctx.fillText(captchaText,20,25);
+}
+
+function verifyCaptcha(){
+let input = document.getElementById("captchaInput").value;
+
+if(input === captchaText){
+return true;
+}else{
+alert("Captcha incorrect ❌");
+generateCaptcha();
+return false;
+}
+}
+
+window.onload = generateCaptcha;
